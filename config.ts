e4eb737075821559f1fff82060e910c73274d2b9
EsmeeIDEMS <esmee@idems.international>
2024-10-25 18:05:28 +0100

    update config to point at geoffrey activity library

--- config.ts
+++ config.ts
@@ -11,7 +11,8 @@ config.google_drive.sheets_folder_ids = [
   "19wSspWYMbRc75een-kS0q0aq24--75u8", // library_app_menu
   "1UXVz71HniwdtklFnGUEBzzj8ZHI9oQVo", // library_field_values
   "1Y8uC9-rqQtsjQgUfeX9qp-vNzsFDUQFU", // kids_global
-  "1pzgmUypvXYJS_ZcC1WmiHM5f4y2QYKsx" // kids_kw
+  "1pzgmUypvXYJS_ZcC1WmiHM5f4y2QYKsx", // kids_kw
+  "1hM8QKhKEM94iFlzuTLh1AMAWTXZdLtbO" // geoffrey activity library
 ];
 
 config.google_drive.assets_folder_ids = ["1abaL1QGd33NqqLoKuo2t9fVWKmh5ouM9", "1d7Xf_k-kCiQsTGlpSAJnVLTTX5zTVvnp"];
